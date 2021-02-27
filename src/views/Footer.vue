471abb6ceadfe6f2d6901c44776774c3a2d409eb
Chris Michael <chris.perez@upr.edu>
2021-02-27 09:17:02 -0400

    the Footer view has been corrected, and styles have been applied

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,40 +1,31 @@
 <template>
-  <div class="container">
-    <div class="clearfix"></div>
-    <div id="footer">
-      <div class="container">
-        <div class="footer-about">
-          <div class="footer-fa-text">
-            <div class="footer-fa-menu">
-              <a href="#"> PopcornTime Show does not store any files on our server, we only linked to the media which is hosted on 3rd party services.</a>
-            </div>
-          </div>
-          <div class="footer-fa-menu">
-            <span class="space">
-              <router-link to="">v{{version}}</router-link>
-            </span>
-          </div>
-        </div>
-      </div>
+  <footer id="footer">
+    <br />
+    <div>
+      <p>
+        PopcornTime Show does not store any files on our server, we only linked
+        to the media which is hosted on 3rd party services.<br />
+      </p>
+      <p>version {{ version }}</p>
     </div>
-  </div>
+  </footer>
 </template>
 
 <script>
-  import { reactive } from "@vue/composition-api";
+import { reactive } from "@vue/composition-api";
 
-  export default {
-    name: "Footer",
-    setup() {
-      const doc = reactive({
-        author: require("../../package.json").author,
-        version: require("../../package.json").version,
-        year: new Date().getFullYear()
-      });
+export default {
+  name: "Footer",
+  setup() {
+    const doc = reactive({
+      author: require("../../package.json").author,
+      version: require("../../package.json").version,
+      year: new Date().getFullYear()
+    });
 
-      return {
-        ...doc
-      };
-    }
-  };
+    return {
+      ...doc
+    };
+  }
+};
 </script>
